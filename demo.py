f0ce284db5392615e1b0668d9526b342e1d2db24
shivampb <sivm0987@gmail.com>
2025-06-02 17:00:24 +0530

    learning

---
+++ demo.py
@@ -0,0 +1,22 @@
+import json
+
+
+def load_data():
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data
+
+
+def videw_data(patient_id: str):
+    data = load_data()
+    if patient_id in data:
+        return data[patient_id]
+    return {"error": "Patient not found"}
+
+
+# print(videw_data("P005"))
+
+
+def data():
+    data = load_data()
+    return data.values()
--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from typing import Union
-
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Query
+import json
 
 app = FastAPI()
+
+
+def load_data():
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data
 
 
 @app.get("/")
@@ -10,6 +15,51 @@ def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}/{q}")
-def read_item(item_id: str, q:str | int  ):
-    return {"item_id": item_id, "q": q}
+@app.get("/about")
+def read_about():
+    return {"About": "this is an about page "}
+
+
+@app.get("/view")
+def view_all_data():
+    data = load_data()
+    return data
+
+
+@app.get("/view/{patient_id}")
+def view_data(patient_id: str):
+    data = load_data()
+
+    if patient_id in data:
+        return data[patient_id]
+    raise HTTPException(status_code=404, detail="patient not found")
+
+
+@app.get("/sort")
+def sort_patients(
+    sort_by: str = Query(
+        ..., description="Sort on the basis of weight, height and bmi"
+    ),
+    order: str = Query("asc", description="Sort in asc or desc order"),
+):
+    valid_sort_fields = ["weight", "height", "bmi", "age"]
+
+    if sort_by.strip() not in valid_sort_fields:
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid field to sort by select from {valid_sort_fields}",
+        )
+
+    if order.strip() not in ["asc", "desc"]:
+        raise HTTPException(
+            status_code=400,
+            detail="Invalid field to order by select from ['asc', 'desc']",
+        )
+
+    data = load_data()
+    sort_order = True if order == "desc" else False
+    return sorted(
+        data.values(),
+        key=lambda x: x[sort_by],
+        reverse=sort_order,
+    )
